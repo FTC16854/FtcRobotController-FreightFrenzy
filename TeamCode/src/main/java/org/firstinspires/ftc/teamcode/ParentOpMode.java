4b1a3ee49ac923b2eb91e2c75bfced1adb5cc178
progam desk <ftc16854@notredamehighschool.com>
2021-11-02 19:51:38 -0400

    We worked on drive train, emergency buttons, learned  booleans, hard ware and controls, learned about teleop. -AB, JB, MdP

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParentOpMode.java
@@ -0,0 +1,245 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Original FTC opmode header block
+ *
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ **/
+
+/** Parent OpMode Class
+ * All Teleop and Autonomous OpModes should inherit from (extend) ParentOpMode.
+ * Each child/subclass OpMode should have its own unique runOpMode() method that will
+ * override the ParentOpMode runOpMode() method.
+ **/
+
+@TeleOp(name="Parent Opmode", group="Linear Opmode")
+@Disabled
+public class ParentOpMode extends LinearOpMode {
+
+    // Declare OpMode members, hardware variables
+    public ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor rightFront = null;
+    private DcMotor rightBack = null;
+    private DcMotor leftFront = null;
+    private DcMotor leftBack = null;
+    //private CRServo intakeServo = null;
+    //private Servo shooterFlipper = null;
+
+    //Other Global Variables
+    //put global variables here...
+    //
+
+    public void initialize(){
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        rightFront = hardwareMap.get(DcMotor.class, "rf_drive");
+        rightBack = hardwareMap.get(DcMotor.class, "rb_drive");
+        leftBack = hardwareMap.get(DcMotor.class, "lb_drive");
+        leftFront = hardwareMap.get(DcMotor.class, "lf_drive");
+        //intakeServo = hardwareMap.get(CRServo.class, "intake_servo");
+        //shooterFlipper = hardwareMap.get(Servo.class,"shooterFlipper_servo");
+
+        //Set motor run mode (if using SPARK Mini motor controllers)
+        //
+
+        //Set Motor  and servo Directions
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+
+        //intakeServo.setDirection(CRServo.Direction.REVERSE);
+        //shooterFlipper.setDirection(Servo.Direction.REVERSE);
+
+        //Set range for special Servos
+        //wobbleLift.scaleRange(0.15,.85); //Savox PWM range is between 0.8 and 2.2 ms. REV Hub puts out 0.5-2.5ms.
+
+        //Set brake or coast modes.
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);//BRAKE or FLOAT (Coast)
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        //Update Driver Station Status Message
+        telemetry.addData("Status:", "Initialized");
+        telemetry.update();
+    }
+
+
+    /**
+     * runOpMode() will be overridden in child OpMode.
+     * Basic structure should remain intact (init, wait for start, while(opModeIsActive),
+     * Additionally, Emergency conditions should be checked during every cycle
+     */
+    @Override
+    public void runOpMode() {
+
+        initialize();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+        //releaseLatch();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            //code here should never actually execute.
+            // This function will be be overridden by child opmode classes
+
+
+            //include emergency stop check in all runOpMode() functions/methods
+            if(emergencyStopped()){
+                break;
+            }
+
+            telemetry.update();
+        }
+    }
+
+    /*****************************/
+    //Controls should be mapped here to avoid
+    //CONTROLLER MAP
+    //Thumbsticks
+    public double left_sticky_x(){
+        return gamepad1.left_stick_x;
+    }
+
+    public double left_sticky_y(){
+        return -gamepad1.left_stick_y;
+    }
+
+    public double right_sticky_x(){
+        return gamepad1.right_stick_x;
+    }
+
+    public double right_stick_y(){
+        return -gamepad1.right_stick_y;
+    }
+
+    //Buttons
+
+    public boolean emergencyButtons (){
+        return  gamepad1.x&&gamepad1.y;
+    }
+
+
+   /* public boolean shootButton(){
+        if((gamepad1.right_trigger>.25)||(gamepad2.right_trigger>.25)){
+            return true;         // Converts analog triggers into digital button presses (booleans)
+        }
+        else{
+            return false;
+        }
+    }
+*/
+
+    /****************************/
+    // Emergency Stop Function
+    public boolean emergencyStopped(){
+        if (emergencyButtons()) {
+            //stop all motors, servos, etc.
+            stopDrive();
+
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+
+    /*****************************/
+    //Drive Methods
+
+    // Assign left and right drive speed using arguments/parameters rather than hardcoding
+    // thumb stick values inside function body. This will allow tank drive to be reused for
+    // autonomous programs without additional work
+    public void tankdrive(double left, double right){
+
+    }
+
+    public void stopDrive(){
+        tankdrive(0,0 );
+    }
+
+
+
+
+
+    /*****************************/
+    //More Methods (Functions)
+
+
+    /*****************************/
+    //Encoder Functions
+   /*
+    public double getLeftVerticalEncoder(){
+        return rightFront.getCurrentPosition();
+    }
+    */
+
+
+}
+
+        //TODO
+        //  Hardware map
+        //  Controls map
+        //  Tank Drive Function
+
+        //Encoder Stuff
+        //  Odometry Wheels
+        //      9192 Counts per revolution
+
+
