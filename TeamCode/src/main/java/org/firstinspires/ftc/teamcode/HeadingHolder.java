3ed735953f669c9f5ff68f9489dbc915b8819e21
progam desk <ftc16854@notredamehighschool.com>
2021-12-30 11:57:17 -0500

    Cleaned up some spelling/formatting

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HeadingHolder.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+public class HeadingHolder {
+    private double OffsetofTheHeading = 0;
+
+    public void SetOffsetOfTheHeading( double InputHeading){
+        OffsetofTheHeading = InputHeading;
+    }
+
+
+
+    //TODO:
+    // Create getter method/function
+}
